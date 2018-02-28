ab118ce40e65943c0acde839ac064b13a22c0718
AbelM <abelm@awards.co.za>
2018-02-28 11:23:15 +0200

    first commit

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(App\Todo::class)->create(['email'=> 'abelmhla@gmail.com']);
+
+    }
+}
